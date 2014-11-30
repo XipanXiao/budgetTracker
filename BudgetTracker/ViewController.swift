6c3a190d6fee80df5ee201073d591f8a20829428
xxp <xxp@xxp-macbookpro.roam.corp.google.com>
2014-11-29 21:13:55 -0800

    Initial UI layout.

--- BudgetTracker/ViewController.swift
+++ BudgetTracker/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UIPickerViewDataSource {
+    let categories = ["Daily", "Education", "Clothing", "Entertainment"]
+    let purchaseDate = "Purchage date: "
+    @IBOutlet weak var dateLabel: UITextField!
+    @IBOutlet weak var category: UITextField!
+    @IBOutlet weak var expenseInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var DatePickerView: UIDatePicker = UIDatePicker()
+        DatePickerView.datePickerMode = UIDatePickerMode.Date
+        dateLabel.inputView = DatePickerView
+        DatePickerView.addTarget(self, action: Selector("dateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        dateLabel.text = "\(purchaseDate) \(dateToString(NSDate()))"
+        
+        var categoryPickerView: UIPickerView = UIPickerView()
+        categoryPickerView.dataSource = self
+        category.inputView = categoryPickerView
+        category.text = categories[0]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dateToString(date: NSDate) -> NSString {
+        var dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        //dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        return dateFormatter.stringFromDate(date)
+    }
 
+    @IBAction func dateChanged(datePicker: UIDatePicker!) {
+        dateLabel.text = "\(purchaseDate) \(dateToString(datePicker.date))"
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        dateLabel.resignFirstResponder()
+        category.resignFirstResponder()
+        expenseInput.resignFirstResponder()
+    }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return categories[row]
+    }
 }
 
